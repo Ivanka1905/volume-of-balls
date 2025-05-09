8c6c34fd3493d7cec544f8728a99bde375b6205f
Ivanka <IvNagornyuk@gmail.com>
2025-05-10 00:18:44 +0300

    add const

--- src/script.js
+++ src/script.js
@@ -1,10 +1,11 @@
 'use strict';
-
-let firstRadius = 0.5 / 2 - 0.01;
+const DIAMETER = 0.5;
+const WALL = 0.01;
+let firstRadius = DIAMETER / 2 - WALL;
 let result = 0;
 
 for (let i = 0; i < 12; i++) {
-  const radius = firstRadius + i*0.01;
+  const radius = firstRadius + i*WALL;
   const volume = (4 * Math.PI * Math.pow(radius, 3)) / 3;
   result += volume;
 }
